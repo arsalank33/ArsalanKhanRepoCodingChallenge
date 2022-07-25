442f77e92365e15439fc37e2315ae05da2de82a7
Acer-PC <Acer-PC@10.0.0.240>
2022-07-24 22:17:03 -0600

    Added my branch label

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 		try {
 			VBox root = new VBox();
 			root.getChildren().add(new Label("Abil's Label"));
-			
+			root.getChildren().add(new Label("Abil's branch"));
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setTitle("Arsalan's Project");
 			primaryStage.setScene(scene);
